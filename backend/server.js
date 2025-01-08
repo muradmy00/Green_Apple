40e4b9bf66351bfe852de9d97ec39983a10aa55c
muradmy00 <murad.my00@gmail.com>
2025-01-08 16:51:27 +0600

    Commit in Jan 8 2025

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+
+// App Config
+
+const app = express()
+
+const port = process.env.PORT || 5000
+
+connectDB()
+connectCloudinary()
+
+// Middleware
+
+app.use(express.json())
+app.use(cors())
+
+
+// Api End point
+
+
+app.get('/',(req,res)=>{
+
+    res.send("API Working")
+})
+
+app.listen(port,()=> console.log('Server Started On PORT :'+ port))
